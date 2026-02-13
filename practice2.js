45eb68626c87a0c52d25cb29a4c08c53f2c78383
draxiel28 <lexerdsotnas@gmail.com>
2026-02-13 17:15:12 +0800

    FizzBuzz pRactice

---
+++ practice2.js
@@ -0,0 +1,13 @@
+for (let i = 1; i < 21; i++) {
+  let j = i;
+  if (i % 3 === 0 && i % 5 === 0) {
+    j == "FizzBuzz";
+  } else if (i % 3 === 0) {
+    j == "Fizz";
+  } else if (i % 5 === 0) {
+    j == "Buzz";
+  }
+  //document.getElementById("num").innerHTML = `${i}`;
+
+  console.log(i);
+}
