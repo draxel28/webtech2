06aee91303774d40f058d07cf88e285b5661278e
draxiel28 <lexerdsotnas@gmail.com>
2026-02-15 11:20:38 +0800

    added linked list accessing a node using .next

--- practice2.js
+++ practice2.js
@@ -1,4 +1,4 @@
-for (let i = 1; i < 51; i++) {
+/* for (let i = 1; i < 51; i++) {
   let j = i;
   if (i % 3 === 0 && i % 5 === 0) {
     j == "FizzBuzz";
@@ -11,3 +11,36 @@ for (let i = 1; i < 51; i++) {
 
   console.log(i);
 }
+  */
+
+/*function check(a, x) {
+  
+  for(let i = 0; i <= a.list; i++){
+    let isTrue = x === a[0] ? 'true' : 'false'
+    
+    return isTrue;
+  }
+}
+check([10,20,30,40],10);*/
+
+/*
+function litres(time) {
+  let litres = Math.floor(time / 0.5);
+
+  console.log(litres);
+}
+  */
+
+// accessing node
+function getNthNode(head, n) {
+  let current = head;
+  let count = 1;
+
+  while (current !== null && count < n) {
+    current = current.next;
+    count++;
+  }
+
+  return current;
+}
+getNthNode(List, 4);
