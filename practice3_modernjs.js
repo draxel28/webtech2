315cb0aee1c8f385dd9126a339370d39168f6161
draxiel28 <lexerdsotnas@gmail.com>
2026-02-17 11:40:26 +0800

    practice using reduce,map,filter and arrow function

---
+++ practice3_modernjs.js
@@ -0,0 +1,31 @@
+const users = [
+  { name: "Drexel", age: 21, email: "drex@gmail.com" },
+  { name: "Anna", age: 17, email: "anna@gmail.com" },
+  { name: "Mark", age: 25, email: "mark@gmail.com" },
+];
+
+//Counting How Many Adults
+
+let numberAdults = (users) => {
+  let adultAge = users
+    .filter((user) => user.age > 18)
+    .map((user) => user.age)
+    .reduce((acc, curr) => acc + 1, 0);
+
+  return adultAge;
+};
+
+console.log(numberAdults(users)); //Output: 2
+
+//Getting youngest user
+
+let youngestAge = (users) => {
+  let age = users.reduce(
+    (acc, curr) => (curr.age <= acc ? curr.age : acc),
+    users[0].age
+  );
+
+  return age;
+};
+
+console.log(youngestAge(users)); //Output: 17
