ea685331b11acd6dfc5f44b1c55e56569a0800c7
draxiel28 <lexerdsotnas@gmail.com>
2026-02-18 08:04:02 +0800

    added reduce and sort practice

--- practice3_modernjs.js
+++ practice3_modernjs.js
@@ -48,3 +48,54 @@ let allNames = (products) => {
 };
 
 console.log(allNames(products)); //Output: "Phone, Laptop, Keyboard"
+
+//Getting names of products with price >= 300------------------------
+
+let highPrices = (products) => {
+  let product = products
+    .filter((prod) => prod.price >= 300)
+    .map((prod) => prod.name)
+    .toString();
+
+  return product;
+};
+
+console.log(highPrices(products)); //Output: "Phone, Laptop"
+
+//Getting total price of all products------------------------
+
+let totalPrice = (products) => {
+  let res = products
+    .map((prod) => prod.price)
+    .reduce((price, curr) => curr + price, 0);
+
+  return res;
+};
+console.log(totalPrice(products)); //Output: 1800
+
+//Getting name of the product with the smallest price----------------
+
+let smallestPrices = (products) => {
+  let product = products.reduce(
+    (prod, curr) => (prod.price < curr.price ? prod : curr),
+    products[0].price
+  );
+
+  return product.name;
+};
+
+console.log(smallestPrices(products)); //Output: "Keyboard"
+
+//Getting names of the 3 cheapest products----------------
+
+let cheapestToExpensive = (products) => {
+  let result = products
+    .slice()
+    .sort((a, b) => a.price - b.price)
+    .slice(0, 3)
+    .map((prod) => prod.name + " : " + prod.price)
+    .toString();
+
+  return result;
+};
+console.log(cheapestToExpensive(products)); //Output: "Keyboard : 100, Phone : 500, Laptop : 1200"
